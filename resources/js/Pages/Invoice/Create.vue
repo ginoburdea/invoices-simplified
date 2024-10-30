aa3676ee21c90602303b3399215815f6a80b300d
Gino Burdea <burdea.gino@gmail.com>
2024-10-30 12:16:53 +0200

    feat: add create invoice page (empty)

---
+++ resources/js/Pages/Invoice/Create.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Create invoice" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Create invoice
+            </h2>
+        </template>
+    </AuthenticatedLayout>
+</template>
+
+<style></style>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -24,4 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::resource('invoices', InvoiceController::class)->only(['create'])->middleware(['auth', 'verified']);
+
+require __DIR__ . '/auth.php';
