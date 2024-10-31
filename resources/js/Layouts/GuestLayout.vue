86a1f227ade9da20760e3db5290057dd2c4746fe
Gino Burdea <burdea.gino@gmail.com>
2024-10-31 11:19:42 +0200

    refactor: extract v-card to a css class for reusability

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
     <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
+        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 px-4"
     >
         <div>
             <Link href="/">
@@ -13,9 +13,7 @@ import { Link } from '@inertiajs/vue3';
             </Link>
         </div>
 
-        <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
-        >
+        <div class="v-card mt-6 w-full overflow-hidden sm:max-w-md">
             <slot />
         </div>
     </div>
