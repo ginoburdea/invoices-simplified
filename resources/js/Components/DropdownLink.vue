cf70c1cfa1ce4c155dd0369c1cfb754a77ad3a29
Gino Burdea <burdea.gino@gmail.com>
2024-10-31 13:17:06 +0200

    feat: make the DropdownLink's href argument optional

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
+import { Link } from "@inertiajs/vue3";
 
 defineProps<{
-    href: string;
+    href?: string;
 }>();
 </script>
 
 <template>
-    <Link
-        :href="href"
-        class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
-    >
+    <Link :href="href" class="dropdown-link" v-if="href">
         <slot />
     </Link>
+    <a class="dropdown-link" v-else>
+        <slot />
+    </a>
 </template>
+
+<style scoped>
+.dropdown-link {
+    @apply block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none cursor-pointer;
+}
+</style>
