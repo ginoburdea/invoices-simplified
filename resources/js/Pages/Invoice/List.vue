36e24204b7134cfa16d51eab2906641e5a26c9d0
Gino Burdea <burdea.gino@gmail.com>
2024-10-31 19:39:34 +0200

    feat: show a create invoice button

--- resources/js/Pages/Invoice/List.vue
+++ resources/js/Pages/Invoice/List.vue
@@ -3,7 +3,7 @@ import Dropdown from "@/Components/Dropdown.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
 import SafeLink from "@/Components/SafeLink.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { computed, ref } from "vue";
+import { Link } from "@inertiajs/vue3";
 
 defineProps({
     invoices: {
@@ -70,9 +70,28 @@ const selectedSortLabel = sortOptions.filter(
 
     <AuthenticatedLayout>
         <template #header>
-            <h1 class="text-xl font-semibold leading-tight text-gray-800">
-                Invoices
-            </h1>
+            <div class="flex justify-between items-center">
+                <h1 class="text-xl font-semibold leading-tight text-gray-800">
+                    Invoices
+                </h1>
+                <Link
+                    :href="route('invoices.create')"
+                    class="v-icon-button !p-1"
+                >
+                    <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M12 4C11.4477 4 11 4.44772 11 5V11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H11V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19V13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H13V5C13 4.44772 12.5523 4 12 4Z"
+                            fill="currentColor"
+                        />
+                    </svg>
+                </Link>
+            </div>
         </template>
 
         <div class="v-card overflow-x-auto">
