6a7e6d7917c5cc54275b03c71ed9304dcfa2fba0
Gino Burdea <burdea.gino@gmail.com>
2024-10-31 15:13:42 +0200

    feat: add safe link component

---
+++ resources/js/Components/SafeLink.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Link } from "@inertiajs/vue3";
+
+defineProps<{
+    href?: string;
+}>();
+</script>
+
+<template>
+    <Link :href="href" class="v-link" v-if="href">
+        <slot />
+    </Link>
+    <a class="v-link" :class="{ 'v-link--disabled': !href }" v-else>
+        <slot />
+    </a>
+</template>
+
+<style></style>
