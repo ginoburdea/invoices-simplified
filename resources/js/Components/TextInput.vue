846143126a98a09847fd000ed82b7357269cd615
Gino Burdea <burdea.gino@gmail.com>
2024-11-01 13:40:28 +0200

    feat: make the multiline TextInput element change the height automatically

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 
-defineProps({
+const props = defineProps({
     multiline: {
         type: Boolean,
         required: false,
@@ -19,11 +19,36 @@ onMounted(() => {
     }
 });
 
+// Resize the textbox when the "input" variable gets assigned
+watch(input, (input) => {
+    if (props.multiline && input) {
+        textboxOnInput({ target: input });
+    }
+});
+
 defineExpose({ focus: () => input.value?.focus() });
+
+const textboxOnInput = (event: Event) => {
+    const currentHeight = parseInt(
+        (event.target.style.height || "").replace("px", "")
+    );
+
+    // If the element doesn't have a height property inside the style property, add it
+    // If it does and the textbox is overflowing, adjust the height
+    if (isNaN(currentHeight) || currentHeight < event.target.scrollHeight) {
+        event.target.style.height = event.target.scrollHeight + 5 + "px";
+    }
+};
 </script>
 
 <template>
-    <textarea class="v-input" v-model="model" ref="input" v-if="multiline" />
+    <textarea
+        class="v-input"
+        v-model="model"
+        ref="input"
+        v-if="multiline"
+        @input="textboxOnInput"
+    />
     <input class="v-input" v-model="model" ref="input" v-else />
 </template>
 
